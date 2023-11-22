efe7d31d0e6760a262f41f7ec646977e163529c7
jRaimuu <liamsarj4@gmail.com>
2023-11-22 10:43:59 -0700

    Removed some code

--- packages/api/routers/index.ts
+++ packages/api/routers/index.ts
@@ -9,10 +9,6 @@ export const appRouter = router({
     return [1, 2, 4, 5, 6];
   }),
 
-  //   getBruh: publicProcedure.query(() => {
-  //     return { ucid: "30121212"}
-  // }),
-
   createActivity: validateScrape.mutation(( {input} ) => {
     // console.log(input);
     // return { username: input }
@@ -29,50 +25,3 @@ export const appRouter = router({
 
   activities: activitiesRouter // nest activities router inside appRouter
 });
-
-
-
-// Add a new mutation procedure in appRouter
-// export const appRouter = t.router({
-//   // ...other procedures...
-
-//   addScrapedData: t.procedure
-//     .input(ScrapeDataSchema)
-//     .mutation(async ({ input }) => {
-//       // Here you can handle the input data, save it to the database, etc.
-//       const { startTime, endTime } = input;
-
-//       // Placeholder: replace with actual logic to handle the scraped data
-//       console.log(Scraped data received: start at ${startTime}, end at ${endTime});
-
-//       // For example, saving to a database might look like this:
-//       // const newData = await DatabaseModel.create({
-//       //   data: {
-//       //     startTime: input.startTime,
-//       //     endTime: input.endTime
-//       //   }
-//       // });
-
-//       // Return a response, could be the newly created database entry or a success message
-//       return { success: true, message: 'Data added successfully', data: input };
-//     }),
-// });
-
-
-// // In your frontend code
-// const { mutate } = trpc.useMutation(['addScrapedData']);
-
-// const dataSet = {
-//   startTime: '5:00PM',
-//   endTime: '6:00PM'
-// };
-
-// mutate(dataSet, {
-//   onSuccess: (response) => {
-//     console.log(response.message); // 'Data added successfully'
-//   },
-//   onError: (error) => {
-//     console.error(error.message);
-//   },
-// });
-
