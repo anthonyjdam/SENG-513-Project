98f94eaec1d0fe6e026ddc8869d72abe2ccb0253
TenzinDorjee1 <87152720+TenzinDorjee1@users.noreply.github.com>
2023-11-23 23:19:41 -0700

    updated model

--- packages/api/db.ts
+++ packages/api/db.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    return await mongoose.connect('mongodb+srv://opengym:opengym@cluster0.ez83orx.mongodb.net/?retryWrites=true&w=majority');
+    return await mongoose.connect(process.env.MONGO_URI || "https://localhost:8000");
 }
