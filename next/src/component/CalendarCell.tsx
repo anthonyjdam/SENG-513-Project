4c82c9299af11daa95710d6824b8bbbe435532e1
jRaimuu <liamsarj4@gmail.com>
2023-11-23 17:14:55 -0700

    Fixed minor styling issues

--- next/src/component/CalendarCell.tsx
+++ next/src/component/CalendarCell.tsx
@@ -11,7 +11,8 @@ function CalendarCell({ cellKey }: CalendarCellProps) {
         <div
             className={`h-[75px] max-h-[75px] w-full grid grid-rows-4 border-t border-b border-r border-neutral-200 ${cellKey % 7 === 0 ? 'bg-zinc-50' : 'bg-white'}`} //make the first column a different color 
             id={keyString}
-        >
+        >   
+            {/* Cell row */}
             <div className="h-[25%] max-h-[25%] border-t border-neutral-100"></div>
             <div className="h-[25%] max-h-[25%] border-t border-neutral-100"></div>
             <div className="h-[25%] max-h-[25%] border-t border-neutral-100"></div>
