0f016be3c450648769360a174a8d19e69eb93669
anthonyjdam <anthonydam1968@gmail.com>
2023-11-25 01:26:33 -0700

    added duration logic in scraper, fixed dotenv import for db connection

--- packages/api/db.ts
+++ packages/api/db.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import 'dotenv/config'
 
 export async function connectDB() {
     return await mongoose.connect(process.env.MONGO_URI || "https://localhost:8000");
