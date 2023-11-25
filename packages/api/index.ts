4d0b2528f54143f94607c6a83138fcb26edd4ce3
jRaimuu <liamsarj4@gmail.com>
2023-11-25 01:17:16 -0700

    Added dependenies and created files

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -3,21 +3,24 @@ import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const PORT = process.env.port || 5000;
 
-// created for each request
+/*Initialize tRPC API*/
+
 const createContext = ({
 	req,
 	res,
 }: trpcExpress.CreateExpressContextOptions) => ({
 	// TODO: CREATE context for each request where we provide the auth session and the db connection
 }); // no context
-type Context = inferAsyncReturnType<typeof createContext>;
 
-export const t = initTRPC.context<Context>().create();
+type Context = inferAsyncReturnType<typeof createContext>; // infer the return type of the create context function
+export const t = initTRPC.context<Context>().create(); // create an instance of the tRPC server
+export const publicProcedure = t.procedure; // export alias of t.procedure as publicProcedure
+export const router = t.router; // define router based on tRPC instance 
 
-export const router = t.router;
 
 export const appRouter = t.router({
 	//   getUser: t.procedure.input(z.string()).query((opts) => {
@@ -38,30 +41,32 @@ export const appRouter = t.router({
 
 	changeName: t.procedure
 		.input(z.object({ username: z.string() }))
-		.mutation(( { ctx, input } ) => {
-      console.log(input.username);
-    }),
+		.mutation(({ ctx, input }) => {
+			console.log(input.username);
+		}),
 
-  createActivity: t.procedure
-    .input(z.object({
-      activity: z.string(),
-      startTime: z.string(),
-      endTime: z.string(),
-      date: z.string(),
-      location: z.string(),
-  }))
-    .mutation(({ ctx, input }) => {
-      console.log(`client says: ${input.startTime}`)
-    }),
+	createActivity: t.procedure
+		.input(z.object({
+			activity: z.string(),
+			startTime: z.string(),
+			endTime: z.string(),
+			date: z.string(),
+			location: z.string(),
+		}))
+		.mutation(({ ctx, input }) => {
+			console.log(`client says: ${input.startTime}`)
+		}),
 
 	// TODO: Make procedures, ideally in another file for organization
 });
 
-// export type definition of API
+// Export type definition of API
 export type AppRouter = typeof appRouter;
 
-const app = express();
 
+/*Initialize Express server*/
+
+const app = express();
 app.use(cors());
 
 app.use(
@@ -79,3 +84,14 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
 	console.log("listening on port " + PORT);
 });
+
+
+/** Connect to mongoDB database */
+mongoose.connect(process.env.MONGO_URI || "http://localhost:5000/trpc"); // default to localhost
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error)); // log error when failing to connect to databse
+db.once('open', () => console.log("Connected to Databse"));
+db.on('disconnected', () => console.log('Disconnected from MongoDB.'));
+
+app.use(express.json());
