f7909c5a190f0dbe7cb19b7dce10628eb295cc8c
jRaimuu <liamsarj4@gmail.com>
2023-11-26 02:43:07 -0700

    added validtor, client code and procedures

--- next/src/component/NewTest.tsx
+++ next/src/component/NewTest.tsx
@@ -2,36 +2,36 @@ import { trpc } from "@/lib/trpc";
 import React from "react";
 
 function NewTest() {
-	type schedule = {
-		activity: string;
-		startTime: string;
-		endTime: string;
-		date: string;
-		location: string;
-	};
+    type schedule = {
+        activity: string;
+        startTime: string;
+        endTime: string;
+        date: string;
+        location: string;
+    };
 
-	const newSchedule: schedule = {
-		activity: "SomeActivity",
-		startTime: "08:00 AM",
-		endTime: "10:00 AM",
-		date: "2023-11-25",
-		location: "SomeLocation",
-	};
+    const newSchedule: schedule = {
+        activity: "SomeActivity",
+        startTime: "08:00 AM",
+        endTime: "10:00 AM",
+        date: "2023-11-25",
+        location: "SomeLocation",
+    };
 
-	const createSched = trpc.schedule.createSchedule.useMutation();
+    const createSched = trpc.schedule.createSchedule.useMutation();
 
-	return (
-		<div>
-			<button
-				onClick={() => {
+    return (
+        <div>
+            <button
+                onClick={() => {
                     createSched.mutate(newSchedule)
-					console.log(createSched.data);
-				}}
-			>
-				NEW TEST
-			</button>
-		</div>
-	);
+                    console.log(createSched.data);
+                }}
+            >
+                NEW TEST
+            </button>
+        </div>
+    );
 }
 
 export default NewTest;
--- packages/api/routers/schedule.ts
+++ packages/api/routers/schedule.ts
@@ -1,14 +1,25 @@
 import { publicProcedure, router } from "../index";
 import { GymScheduleModel } from "../models/GymSchedule";
-import { validateSchedule } from "../procedures";
+import { validateSchedule, validateScheduleOutput } from "../procedures";
 import mongoose from "mongoose";
+import { ScrapeDataSchemaArray } from "../procedures"
+import { z } from "zod"; // input validator
+
 
 export const scheduleRouter = router({
-	createSchedule: validateSchedule.mutation(async ({ ctx, input }) => {
-		const schedule = new GymScheduleModel(input);
-		await schedule.save();
-		console.log("HIT");
+    createSchedule: validateSchedule.mutation(async ({ ctx, input }) => {
+        const schedule = new GymScheduleModel(input);
+        await schedule.save();
+        console.log("HIT");
 
-		return { resString: "Success creating schedule" };
-	}),
+        return { resString: "Success creating schedule" };
+    }),
+
+    getSchedules: validateScheduleOutput.query(async (opts) => {
+
+        type ScheduleArrayType = z.infer<typeof ScrapeDataSchemaArray>
+        const schedules: ScheduleArrayType = await GymScheduleModel.find();
+
+        return schedules;
+    }),
 });
