e85542a29a3aae5e590ef83ee6bb04614883b7f4
jRaimuu <liamsarj4@gmail.com>
2023-11-26 02:43:07 -0700

    added validtor, client code and procedures

--- packages/api/procedures.ts
+++ packages/api/procedures.ts
@@ -1,15 +1,19 @@
-import { TRPCError } from '@trpc/server'; // error handler
+import { TRPCError } from "@trpc/server"; // error handler
 import { publicProcedure } from "./index"; // trpc instance
-import { z } from 'zod'; // input validator
+import { z } from "zod"; // input validator
 
 export const validateUsername = publicProcedure
-    .input(z.object({ // validate that the username is a string
-        username: z.string(),
-    }))
+    .input(
+        z.object({
+            // validate that the username is a string
+            username: z.string(),
+        })
+    )
     .use((opts) => {
-        if (typeof opts.input.username !== 'string') { // throw error if not of type string
+        if (typeof opts.input.username !== "string") {
+            // throw error if not of type string
             throw new TRPCError({
-                code: 'BAD_REQUEST',
+                code: "BAD_REQUEST",
                 message: "Not of string type",
             });
         }
@@ -26,12 +30,26 @@ const ScrapeDataSchema = z.object({
     location: z.string(),
 });
 
+export const ScrapeDataSchemaArray = z.array(
+    z.object({
+        activity: z.string(),
+        startTime: z.string(),
+        endTime: z.string(),
+        date: z.string(),
+        location: z.string(),
+    })
+);
+
 export const validateSchedule = publicProcedure
     .input(ScrapeDataSchema)
     .use((opts) => {
         //input validation
         return opts.next();
-    })
+    });
 
-
-    
+export const validateScheduleOutput = publicProcedure
+    .output(ScrapeDataSchemaArray)
+    .use((opts) => {
+        //input validation
+        return opts.next();
+    });
