e27e07408eb588e9c305a998049accfb6803b0fb
anthonyjdam <anthonydam1968@gmail.com>
2023-11-26 01:29:31 -0700

    Minor change

--- next/src/component/NewTest.tsx
+++ next/src/component/NewTest.tsx
@@ -1,29 +1,37 @@
-import { trpc } from '@/lib/trpc'
-import React from 'react'
+import { trpc } from "@/lib/trpc";
+import React from "react";
 
 function NewTest() {
+	type schedule = {
+		activity: string;
+		startTime: string;
+		endTime: string;
+		date: string;
+		location: string;
+	};
 
-    type schedule = {
-        activity: string;
-        startTime: string;
-        endTime: string;
-        date: string;
-        location: string;
-    }
+	const newSchedule: schedule = {
+		activity: "SomeActivity",
+		startTime: "08:00 AM",
+		endTime: "10:00 AM",
+		date: "2023-11-25",
+		location: "SomeLocation",
+	};
 
-    const newSchedule: schedule = {
-        activity: "SomeActivity",
-        startTime: "08:00 AM",
-        endTime: "10:00 AM",
-        date: "2023-11-25",
-        location: "SomeLocation",
-    };
+	const createSched = trpc.schedule.createSchedule.useMutation();
 
-    const response = trpc.getHello.useQuery();
-
-    return (
-        <div>NewTest</div>
-    )
+	return (
+		<div>
+			<button
+				onClick={() => {
+                    createSched.mutate(newSchedule)
+					console.log(createSched.data);
+				}}
+			>
+				NEW TEST
+			</button>
+		</div>
+	);
 }
 
-export default NewTest
+export default NewTest;
--- packages/api/routers/schedule.ts
+++ packages/api/routers/schedule.ts
@@ -1,17 +1,14 @@
-import { publicProcedure, router } from "../index"
+import { publicProcedure, router } from "../index";
 import { GymScheduleModel } from "../models/GymSchedule";
-import { validateSchedule } from "../procedures"
+import { validateSchedule } from "../procedures";
 import mongoose from "mongoose";
 
-
 export const scheduleRouter = router({
+	createSchedule: validateSchedule.mutation(async ({ ctx, input }) => {
+		const schedule = new GymScheduleModel(input);
+		await schedule.save();
+		console.log("HIT");
 
-    createSchedule: validateSchedule.mutation(async (req) => {
-        const schedule = new GymScheduleModel(req);
-        await schedule.save();
-        return "Success creating schedule"
-    })
-
-
-
-})
+		return { resString: "Success creating schedule" };
+	}),
+});
