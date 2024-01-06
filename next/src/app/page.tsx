3c7434dbf7ffa6691ea5159cceee26bb91287dc4
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2024-01-05 22:00:45 -0700

    hitting our server on the ec2 instance instead of local host + minor refactors

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -7,55 +7,58 @@ import Sidebar from "@/component/Sidebar";
 import TimesColumn from "@/component/TimesColumn";
 import { Topbar } from "@/component/Topbar";
 import { Schedule } from "@/component/Schedule";
-import { Toaster } from "@/components/ui/toaster"
-import { useToast } from "@/components/ui/use-toast"
-import { ToastAction } from "@/components/ui/toast"
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 import { generateTimes } from "@/lib/utilityFunctions";
+require("dotenv").config();
 
 export default function Home() {
-
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() => {
     return trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:5001/trpc",
+          url: process.env.NEXT_PUBLIC_HTTP_BATCH_LINK as string,
         }),
       ],
     });
   });
 
-  const { toast } = useToast()
+  const { toast } = useToast();
   const [isPopupVisible, setPopupVisible] = useState(true);
 
   const dismissPopupForever = () => {
     // set a flag in local storage to remember that the user dismissed the popup
-    localStorage.setItem('popupDismissed', 'true');
+    localStorage.setItem("popupDismissed", "true");
     setPopupVisible(false);
   };
 
   useEffect(() => {
-    const dismissed = localStorage.getItem('popupDismissed');
+    const dismissed = localStorage.getItem("popupDismissed");
 
     if (dismissed) {
       setPopupVisible(false);
-    } 
-    else { //display popup if local storage isnt false
+    } else {
+      //display popup if local storage isnt false
       toast({
-        title: '👋 Welcome',
+        title: "👋 Welcome",
         description:
-          'Please be aware that this website is experimental and may not present information accurately or as intended. Thank you for your understanding as we work to improve it.',
-        action: <ToastAction onClick={dismissPopupForever} altText="Dismiss forever">Dismiss Forever</ToastAction>,
+          "Please be aware that this website is experimental and may not present information accurately or as intended. Thank you for your understanding as we work to improve it.",
+        action: (
+          <ToastAction onClick={dismissPopupForever} altText="Dismiss forever">
+            Dismiss Forever
+          </ToastAction>
+        ),
       });
     }
-
   }, []);
 
   return (
     <trpc.Provider queryClient={queryClient} client={trpcClient}>
       <QueryClientProvider client={queryClient}>
         <main className="flex flex-row min-h-screen">
-                      <Toaster></Toaster>
+          <Toaster></Toaster>
           <div className="relative">
             <Sidebar />
           </div>
